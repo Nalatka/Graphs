b15912b457fd0eb117125cce2a982325f30c7025
Nalatka <peacediebull1@gmail.com>
2025-05-19 18:34:25 +0500

    EdgeUpdate

--- src/Edge.java
+++ src/Edge.java
@@ -1,4 +1,41 @@
+import java.util.Objects;
+
 public class Edge<Vertex> {
     private Vertex from;
     private Vertex to;
+    private double weight;
+    public Edge(Vertex from, Vertex to, double weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+    public Edge(Vertex from, Vertex to) {
+        this.from = from;
+        this.to = to;
+    }
+    public Vertex getFrom() {
+        return from;
+    }
+    public void setFrom(Vertex from) {
+        this.from = from;
+    }
+    public Vertex getTo() {
+        return to;
+    }
+    public void setTo(Vertex to) {
+        this.to = to;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Edge<?> edge = (Edge<?>) o;
+        return Objects.equals(from, edge.from) && Objects.equals(to, edge.to);
+    }
 }
