08442210ab5e6220a19b3803b4f359e269002d1d
Nalatka <peacediebull1@gmail.com>
2025-05-19 18:23:16 +0500

    VertexUpdate

--- src/Vertex.java
+++ src/Vertex.java
@@ -17,8 +17,24 @@ public class Vertex<V> {
     public void setData(V data) {
         this.data = data;
     }
+    public Map<Vertex<V>, Double> getAdjacenctVertices() {
+        return adjacenctVertices;
+    }
+
+    public void setAdjacenctVertices(Map<Vertex<V>, Double> adjacenctVertices) {
+        this.adjacenctVertices = adjacenctVertices;
+    }
+
     @Override
     public String toString() {
         return data.toString();
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Vertex) {
+            Vertex<?> other = (Vertex<?>) obj;
+            return this.data.equals(other.data);
+        }
+        return false;
     }
 }
